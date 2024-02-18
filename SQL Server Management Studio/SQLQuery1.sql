30360f3f8de28515e17355031c8d09ac8c1217aa
Franco Maximiliano Furlani <francofurlani@gimail.com>
2024-02-18 19:45:07 -0300

    18-2 b

--- SQL Server Management Studio/SQLQuery1.sql
+++
@@ -1,7 +0,0 @@
-use pubs
-
--- 1) Realizar una consulta que muestre los nombres de los autores y editores ubicados en la misma ciudad
-
-select au_fname as apellido, au_lname as nombre, pub_name as editor
-from authors inner join publishers
-on authors.city = publishers.city
--- SQL Server Management Studio/SQLQuery3.sql
+++
@@ -1,2 +0,0 @@
-select top 8 * from Empleados;
-select top 50 percent * from Empleados where activo = 'no';
